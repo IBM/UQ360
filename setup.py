266cf81c6cae59df2b86c73a971dbe8e2c715236
Prasanna Sattigeri <pronics2004@users.noreply.github.com>
2021-06-10 12:49:28 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='uq360',
     version='0.1',
-    url='https://github.ibm.com/UQ360/UQ360',
+    url='https://github.com/IBM/UQ360',
     license='Apache License 2.0',
     author='uq360 developers',
     author_email='uq360@us.ibm.com',
