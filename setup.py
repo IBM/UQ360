cf02a8aa4242ec8403ec834576d8aa284cb964a2
Prasanna Sattigeri <psattig@Prasannas-MacBook-Pro.local>
2021-06-18 10:02:03 -0400

    Updated url in setup.py.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='uq360',
-    version='0.1',
+    version='0.1.1',
     url='https://github.com/IBM/UQ360',
     license='Apache License 2.0',
     author='uq360 developers',
