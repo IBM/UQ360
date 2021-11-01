c7ce326a1c3aa144f4caaeec90b0def79727863d
Aditya Jain <adityajain93@gmail.com>
2021-11-01 13:34:05 -0500

    Adding functional test cases for calibrated porbabilities to be better

--- tests/test_ClassificationCalibration.py
+++ tests/test_ClassificationCalibration.py
@@ -8,14 +8,20 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 import logging
-
+from sklearn.metrics import brier_score_loss
+from sklearn.utils._testing import (
+    assert_array_almost_equal,
+    assert_almost_equal,
+    assert_array_equal,
+    ignore_warnings,
+)
 tf.get_logger().setLevel(logging.ERROR)
 
 
 class TestCalibratedClassifier(TestCase):
 
     def setUp(self):
-        self.num_classes = 4
+        self.num_classes = 2
         X, y = self._generate_mock_data(n_samples  = 10000,
              n_classes= self.num_classes, n_features = 8) 
     
@@ -27,25 +33,40 @@ class TestCalibratedClassifier(TestCase):
         self.y_prod = y_prod
         self.model = self._train_model(x_train, y_train)
 
-    def test_calibrated_classifier(self):
-        
-        # Check if calibrated classifier runs with probs method
-        calib_model_probs = ClassificationCalibration(self.num_classes, 
-                fit_mode = 'probs')
-        # Fitting probs mode using probability scores
-        calib_model_probs.fit(self.model.predict_proba(self.x_test), self.y_test)
-        res_probs = calib_model_probs.predict(self.model.predict_proba(self.x_test))
-
-        # check if features fit_mode runs model run
-
-        ## Gives an error right now
-        calib_model_feat = ClassificationCalibration(self.num_classes, fit_mode = 'features',
-                base_model_prediction_func = self.model.predict_proba)
-
-        calib_model_feat.fit(self.x_test, self.y_test)
-        res_feat = calib_model_feat.predict(self.x_prod)
 
 
+    def test_calibrated_classifier(self):    
+        # Predictions for uncalibrated classifier
+        y_pred = self.model.predict(self.x_prod)
+        y_proba = self.model.predict_proba(self.x_prod)
+        for method in ["isotonic", "sigmoid"]:
+            # Estimator runs for fit_mode = 'probs'
+            calib_model_probs = ClassificationCalibration(self.num_classes, 
+                    fit_mode = 'probs', method = method)
+            calib_model_probs.fit(self.model.predict_proba(self.x_test), self.y_test)
+            res_probs = calib_model_probs.predict(self.model.predict_proba(self.x_prod))
+            # Compare results with uncalibrated classifier
+            print("Unaclibrate ",y_proba.shape)
+            print("calibrated ",res_probs.y_prob.shape)
+
+            assert_array_equal(y_pred, res_probs.y_pred)
+            assert brier_score_loss(self.y_prod, y_proba[:,0]) >= brier_score_loss(
+                self.y_prod, res_probs.y_prob[:,0])
+
+            # Estimator runs for fit_mode = 'features'
+            calib_model_feat = ClassificationCalibration(self.num_classes, fit_mode = 'features',
+                    base_model_prediction_func = self.model.predict_proba,
+                    method = method)
+
+       
+
+            calib_model_feat.fit(self.x_test, self.y_test)
+            res_feat = calib_model_feat.predict(self.x_prod)
+
+
+            assert_array_equal(y_pred, res_feat.y_pred)
+            assert brier_score_loss(self.y_prod, y_proba[:,0]) >= brier_score_loss(
+                self.y_prod, res_feat.y_prob[:,0])
 
 
 
