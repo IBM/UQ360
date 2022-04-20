11445035c0c703771e62e26aa45f518b81f5fd05
Francesco Craighero FCR@zurich.ibm.com <fcraighero@cccxc412.pok.ibm.com>
2022-04-20 12:19:19 -0400

    Fixed various errors, added notebook for titan

--- dante/latent_features/extractor.py
+++ dante/latent_features/extractor.py
@@ -1,7 +1,7 @@
 import logging
 import os
 from functools import reduce
-from typing import Callable, Union, list, tuple
+from typing import Callable, Union, Tuple, List
 
 import numpy as np
 import torch
@@ -17,20 +17,20 @@ class ModelFeatureExtractor():
     def __init__(
         self,
         model: Callable,
-        dataloaders: tuple[DataLoader, str],
-        layers: Union[list[list(str)], list(tuple(list(str), str))],
+        dataloaders: Tuple[DataLoader, str],
+        layers: Union[List[List[str]], List[Tuple[List[str], str]]],
         model_name: str,
         out_dir: str = './output',
         loggin_level: str = 'DEBUG', 
-        disable_tqdm=False):
+        disable_tqdm = False):
     
         # Set loggin level
         self.logger = logging.getLogger('feature_extract')
         self.logger.setLevel(loggin_level)
 
         self.model = model
-        self.dataloader = dataloaders
-        self.layer = layers
+        self.dataloaders = dataloaders
+        self.layers = layers
         self.model_name = model_name
         self.out_dir = out_dir
         self.disable_tqdm = disable_tqdm
@@ -44,7 +44,7 @@ class ModelFeatureExtractor():
 
         # If model_name is given, create subfolder
         if self.model_name is not None:
-            out_dir = os.path.join(out_dir, self.model_name)
+            out_dir = os.path.join(self.out_dir, self.model_name)
             os.mkdir(out_dir)
             model_name = self.model_name + '_'
         else:
@@ -55,7 +55,7 @@ class ModelFeatureExtractor():
                 self.get_submodule(submodules_list, get_name=True) for
                 submodules_list in self.layers
             ]
-        elif type(layers[0]) is tuple:
+        elif type(self.layers[0]) is tuple:
             layers = [
                 (self.get_submodule(submodules_list, get_name=False), l_name)
                 for submodules_list, l_name in self.layers
@@ -68,7 +68,7 @@ class ModelFeatureExtractor():
 
             for layer, layer_name in layers:
 
-                latent_features = self.extract_latent_features(dl, layer, layer_name)
+                latent_features = self._extract_latent_features(dl, layer)
                 
                 fname = f"{model_name}latent_{dl_name}_{layer_name}.pt"
                 torch.save(
@@ -105,6 +105,6 @@ class ModelFeatureExtractor():
         )
 
         if get_name:
-            return module, submodules_list.join('_')
+            return module, '_'.join(submodules_list)
         else: 
             return module
