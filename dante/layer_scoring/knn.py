3c4b4e79b116f6f0715383818408dd7c192abbe4
Francesco Craighero <FCR@zurich.ibm.com>
2022-06-15 13:52:04 +0200

    Added knn and faiss.

---
+++ dante/layer_scoring/knn.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+from ..nearest_neighbors.base import BaseNearestNeighbors
+
+
+class Knn:
+    def __init__(
+        self,
+        nearest_neighbors: BaseNearestNeighbors,
+        nearest_neighbors_kwargs={},
+    ):
+        self.nearest_neighbors = nearest_neighbors
+        self.nearest_neighbors_kwargs = nearest_neighbors_kwargs
+
+    def fit(self, X):
+        self.index = self.nearest_neighbors.fit(X, **self.nearest_neighbors_kwargs)
+
+    def score(self, X, k, method="knn"):
+
+        dist, idxs = self.nearest_neighbors.kneighbors(X, k)
+
+        if method == "knn":
+            return np.max(dist, axis=1)
+        elif method == "avg":
+            return np.mean(dist, axis=1)
+        elif method == "lid":
+            return -k / np.sum(np.log(dist / np.max(dist)))
+        elif method is None:
+            return dist, idxs
+        else:
+            raise ValueError()
