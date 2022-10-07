e6210f0fb79b0e055e5da7bbff37d4da0df06fd4
Nicolas Deutschmann <deu@zurich.ibm.com>
2022-10-07 17:10:44 +0200

    converted util functions to FeatureTransformer

--- uq360/utils/transformers/nearest_neighbors/faiss.py
+++ uq360/utils/transformers/nearest_neighbors/faiss.py
@@ -1,13 +1,22 @@
 import faiss
+
 from .base import BaseNearestNeighbors
 
 
-class NearestNeighbors(BaseNearestNeighbors):
-    def __int__(self):
-        super(NearestNeighbors, self).__int__()
+class FAISSNearestNeighbors(BaseNearestNeighbors):
+    def __init__(self):
+        super(FAISSNearestNeighbors, self).__init__()
         self.index = None
+        self.X = None
+        self.fit_kwargs = dict()
+
+    @classmethod
+    def name(cls):
+        return 'faiss_nearest_neighbors'
 
     def fit(self, X, use_gpu=False, **kwargs):
+        self.X = X.copy()
+        self.fit_kwargs = dict(kwargs, use_gpu=use_gpu)
 
         n_features = X.shape[1]
         self.index = faiss.IndexFlatL2(n_features, **kwargs)
@@ -23,3 +32,18 @@ class NearestNeighbors(BaseNearestNeighbors):
         distances, indices = self.index.search(X, n_neighbors)
 
         return distances, indices
+
+    def save(self, output_location=None):
+        self.register_pkl_object(self.X, "X")
+        self.register_pkl_object(self.fit_kwargs, "fit_kwargs")
+        self._save(output_location=output_location)
+
+    def load(self, input_location=None):
+        self._load(input_location=input_location)
+
+        objs, names = self.pkl_registry
+        obj_map = dict(zip(names, objs))
+
+        self.X = obj_map['X']
+        self.fit_kwargs = obj_map['fit_kwargs']
+
